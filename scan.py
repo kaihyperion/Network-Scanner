bf6be4a2ebf2e3ec21c8bbefa12acd537e4bf0d2
kaihyperion <kyperion.workspace@gmail.com>
2021-03-12 03:28:15 -0600

    Finished Part 1. Scan_time works

--- scan.py
+++ scan.py
@@ -22,12 +22,49 @@ json.dump(json_object, f, sort_keys =True, indent=4)
 """
 
 # Extract from the textfile
-url_list = []
 
-filename = sys.argv[1]
 
-with open(filename, 'r') as reader:
-    for line in reader:
-        url_list.append(line.strip('\n'))
+class Scanner:
+    def __init__(self, input_file, output_file):
+        self.filename = input_file
+        self.output_json = output_file
+        self.url_list = []
+        #Create a nested dictionary
+        self.result = {}
 
-print(url_list)
+        # parse through txt file and put url into list.
+        with open(self.filename, 'r') as url_reader:
+            for line in url_reader:
+
+                self.url_list.append(line.strip('\n'))
+
+
+
+        for url in self.url_list:
+            self.result[url]={}
+            self.result[url]["scan_time"] = self.scan_time()
+        with open(self.output_json, 'w') as writer:
+            json.dump(self.result, writer, sort_keys=True, indent = 4)
+
+    def scan_time(self):
+        return time.time()
+
+
+    """ 
+        {url1 : {"scan"_time":1231, "ipv4_addresses":12123...}
+         url2 : {}...
+    """
+
+
+
+
+# It's key should be the domain that were scanned and the values are dictionaries with
+# scanned results.
+
+#Create nested dictionary. programiz.com/python-programming/nested-dictionary
+
+
+main = Scanner(sys.argv[1], sys.argv[2])
+
+
+
